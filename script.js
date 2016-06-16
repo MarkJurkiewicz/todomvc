9df800013388190c46d02f1b813186e189c54caa
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-06-15 22:22:09 -0700

    cleaned up code deleted displayTodos method in original todoList object and removed Display Todos button

--- script.js
+++ script.js
@@ -1,45 +1,27 @@
 var todoList = {
     todos: [],
-    displayTodos: function() {
-        if (this.todos.length === 0) {
-            console.log('Your todo list is empty!');
-        } else {
-            console.log('My Todos: ');
-            for(var i = 0; i < this.todos.length; i++) {
-                if (this.todos[i].completed === true) {
-                    console.log('(x)', this.todos[i].todoText);
-                } else {
-                    console.log('( )', this.todos[i].todoText);
-                }
-            }
-        }
-    },
     addTodo: function(todoText) {
         this.todos.push({
             todoText: todoText,
             completed: false
         });
-        this.displayTodos();
     },
     changeTodo: function(position, todoText) {
         this.todos[position].todoText = todoText;
-        this.displayTodos();
     },
     deleteTodo: function(position) {
         this.todos.splice(position, 1);
-        this.displayTodos();
     },
     toggleCompleted: function(position) {
         var todo = this.todos[position];
         todo.completed = !todo.completed;
-        this.displayTodos();
     },
     toggleAll: function() {
         var totalTodos = this.todos.length;
         var completedTodos = 0;
 
         // Get number of completed todos.
-        for (var i = 0; i < totalTodos; i++) {
+        for (var i = 0; i < totalTodos; i++){
             if (this.todos[i].completed === true) {
                 completedTodos++;
             }
@@ -47,30 +29,26 @@ var todoList = {
         // CASE 1: If everything is true, then make everything false.
         if (completedTodos === totalTodos) {
             // Make everything false.
-            for (var i = 0; i < totalTodos; i++) {
+            for (var i = 0; i < totalTodos; i++){
                 this.todos[i].completed = false;
             }
             // CASE 2: Otherwise, make everything true.
         } else {
-            for (var i = 0; i < totalTodos; i++) {
+            for (var i = 0; i < totalTodos; i++){
                 this.todos[i].completed = true;
             }
 
         }
 
-        this.displayTodos();
     }
 };
 
 var handlers = {
-    displayTodos: function () {
-        todoList.displayTodos();
-    },
-
     addTodo: function () {
         var addTodoTextInput = document.getElementById('addTodoTextInput');
         todoList.addTodo(addTodoTextInput.value);
         addTodoTextInput.value = '';
+        view.displayTodos();
     },
     changeTodo: function () {
         var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
@@ -78,22 +56,25 @@ var handlers = {
         todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
         changeTodoPositionInput.value = '';
         changeTodoTextInput.value = '';
+        view.displayTodos();
     },
     deleteTodo: function () {
         var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
         todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
         deleteTodoPositionInput.value = '';
+        view.displayTodos();
     },
     toggleCompleted: function () {
         var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
         todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
         toggleCompletedPositionInput.value = '';
+        view.displayTodos();
     },
     toggleAll: function () {
         todoList.toggleAll();
+        view.displayTodos();
     }
 };
-
 // responsible for what user sees
 var view = {
     displayTodos: function () {
